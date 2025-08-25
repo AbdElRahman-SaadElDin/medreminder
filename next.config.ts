4d87d8fcccdedb7c321334691b18366a701ee857
AbdElRahman-SaadElDin <131001698+AbdElRahman-SaadElDin@users.noreply.github.com>
2025-08-26 02:59:43 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,15 @@
 import type { NextConfig } from "next";
 
-const base = "/medreminder"; // <-- your repo name
-
 const nextConfig: NextConfig = {
-  output: "export",          // static export => ./out
-  basePath: base,
-  assetPrefix: base + "/",
+  output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "/" },
-      { protocol: "https", hostname: "assets.zyrosite.com", pathname: "/" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "assets.zyrosite.com", pathname: "/**" },
     ],
   },
-  eslint: { ignoreDuringBuilds: true }, // optional, to avoid failing CI on lint
+  eslint: { ignoreDuringBuilds: true }, // optional, to not fail CI on lint
 };
 
 export default nextConfig;
